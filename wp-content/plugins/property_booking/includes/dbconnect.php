e031c1f11d0054366d7fb7084a11defb726b7c34
Javarn Tromp de Haas <83565578+JavarnTr@users.noreply.github.com>
2023-05-29 15:52:17 +1200

    Delete wp-content directory

--- wp-content/plugins/property_booking/includes/dbconnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $servername = "localhost";
-    $username = "admin";
-    $password = "admin1234";
-    $dbname = "property_booking";
-
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-?>
