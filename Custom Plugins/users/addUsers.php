645b8caad1fccabe4b4f798defc36fd442d6e639
Sambo177 <samuel78kennedynz@gmail.com>
2023-05-30 13:43:06 +1200

    Update addUsers.php
    Baseline to create user accounts.

--- Custom Plugins/users/addUsers.php
+++ Custom Plugins/users/addUsers.php
@@ -2,16 +2,19 @@
     include('includes/dbconnect.php');
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $address = $_POST["address"];
-        $price = $_POST["price"];
-        $city = $_POST["city"];
-        $specifications = $_POST["specifications"];
-        $images = $_POST["images"];
+        $firstName = $_POST["firstName"];
+        $lastName = $_POST["lastName"];
+        $phoneNumber = $_POST["phoneNumber"];
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+		$address = $_POST["address"];
+		$licenceNumber = $_POST["licenceNumber"];
+		$photoIdentification = $_POST["photoIdentification"];
 
-        $sql = "Insert into properties (address, price, city, specifications, images) Values ('$address', '$price', '$city', '$specifications', '$images')";
+        $sql = "Insert into users (firstName, lastName, phoneNumber, email, password, address, licenceNumber, photoIdentification) Values ('$firstName', '$lastName', '$phoneNumber', '$email', '$password', '$address', '$licenceNumber', '$photoIdentification')";
         
         if ($conn->query($sql) === TRUE) {
-            echo "Property added";
+            echo "User added added";
         } else {
             echo "An error occured";
         }
@@ -20,27 +23,36 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Create a listing</title>
+    <title>Create a user account</title>
 </head>
 <body>
     <form method="POST" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
     
-    <label for="address">Address:</label>
+    <label for="firstName">First Name:</label>
+    <input type="text" name="firstName" id="firstName" required><br>
+
+    <label for="lastName">Last Name:</label>
+    <input type="text" name="lastName" id="lastName" required><br>
+
+    <label for="phoneNumber">Phone Number:</label>
+    <input type="text" name="phoneNumber" id="phoneNumber" required><br>
+
+    <label for="email">Email:</label>
+    <input type="text" name="email" id="email" required><br>
+
+    <label for="password">Password:</label>
+    <input type="text" name="password" id="password" required><br>
+	
+	<label for="address">Address:</label>
     <input type="text" name="address" id="address" required><br>
+	
+	<label for="password">LicenceNumber:</label>
+    <input type="text" name="licenceNumber" id="licenceNumber" required><br>
+	
+	<label for="photoIdentification">Photo Identification:</label>
+    <input type="text" name="photoIdentification" id="photoIdentification" required><br>
 
-    <label for="price">Price:</label>
-    <input type="text" name="price" id="price" required><br>
-
-    <label for="city">City:</label>
-    <input type="text" name="city" id="city" required><br>
-
-    <label for="specifications">Specifications:</label>
-    <input type="text" name="specifications" id="specifications" required><br>
-
-    <label for="images">Images:</label>
-    <input type="text" name="images" id="images" required><br>
-
-    <input type="submit" value="Add Property">
+    <input type="submit" value="Create Account">
     </form>
 
 </body>
