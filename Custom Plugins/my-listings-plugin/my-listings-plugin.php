99538d21919ab31ffe16623a8ec94d00bc22c009
Sambo177 <samuel78kennedynz@gmail.com>
2023-06-21 16:59:54 +1200

    Update my-listings-plugin.php
    Include database connection

--- Custom Plugins/my-listings-plugin/my-listings-plugin.php
+++ Custom Plugins/my-listings-plugin/my-listings-plugin.php
@@ -72,6 +72,8 @@ function accounts_shortcode() {
 //Allow the user to log into accounts
 function accountSignIn_shortcode() {
     //Login users
+    include('includes/dbconnect.php');
+    
 	if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["login"])) {
         $email = $_POST["email"];
         $password = $_POST["password"];
